53dfa8987feeed06877190f7a9592fe9e4a0e77e
gaturro200431 <101894254+gaturro200431@users.noreply.github.com>
2022-03-24 17:30:54 -0500

    Add files via upload
    24/03/22

---
+++ Usminia11/actividad.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+</head>
+<body>
+    <h1> Hola soy un titulo </h1>
+    <?php
+    //INICIA CODIGO EN PHP
+    // esto es un comentario 
+    /* Todo lo que esta aqui dentro 
+    se considera un comentario
+    */
+     // COMANDO ECHO DEVUELVE EL VALOR 
+     echo "hola soy un parrafo "; 
+     echo "<br>"; // salio de linea 
+     echo 3+3;    /* realiza una operacion en php
+                    que no se puede en html */
+    // TERMINA EN PHP 
+    ?> 
+    </body> 
+    </html>                
---
+++ Usminia11/actividad2.php
@@ -0,0 +1,11 @@
+<?php
+// definamos una varible con un texto 
+$miVariable = "Hola variable";
+// visualizamos la variable en el explorador 
+echo $miVariable;
+// otro ejemplo variable miValor igual a 2
+echo"<br>";
+$miValor = 2;
+// Visualizamos sin embargo realizamos una multiplicacion * 3
+echo 3*$miValor;
+?>
